cbcdf2e7d89c2a86644d7541778812d02e07c579
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-07-29 16:52:49 -0500

    updating editServlet still need to finish

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -14,19 +14,82 @@ import java.io.IOException;
 public class EditAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //grab ad by ID from MySqlAdsDao create a method
+
+//
         Long oneAd = Long.parseLong(request.getParameter("singleAd"));
         Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
+
+        request.setAttribute("existingTitle", singleAd.getTitle());
+        request.setAttribute("existingDescription", singleAd.getDescription());
+
+
+        //grab ad by ID from MySqlAdsDao create a method
+//        Long oneAd = Long.parseLong(request.getParameter("singleAd"));
+//
+//            Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
+//            System.out.println("singleAd.getTitle() = " + singleAd.getTitle());
+//            //ad that gets back set as an attribute do ${};
+//            request.setAttribute("singleAd", singleAd);
+//            request.setAttribute("existingTitle", singleAd.getTitle());
+//            request.setAttribute("existingDescription", singleAd.getDescription());
+//
+//            //======SETTING VALUES IN THE FORM=======//
+//            String title = request.getParameter("title");
+//            String description = request.getParameter("description");
+//
+//
+//            DaoFactory.getAdsDao().edit(singleAd, title, description);
+//            response.sendRedirect("/ads/profile");
+//
+            request.getRequestDispatcher("/WEB-INF/ads/editads.jsp").forward(request, response);
+
+
+//        Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
+//        System.out.println("singleAd.getTitle() = " + singleAd.getTitle());
+//        //ad that gets back set as an attribute do ${};
+//        request.setAttribute("singleAd", singleAd);
+//        request.setAttribute("existingTitle", singleAd.getTitle());
+//        request.setAttribute("existingDescription", singleAd.getDescription());
+//
+//        //======SETTING VALUES IN THE FORM=======//
+//        String title = request.getParameter("title");
+//        String description = request.getParameter("description");
+//
+//
+//        DaoFactory.getAdsDao().edit(singleAd, title, description);
+//        response.sendRedirect("/ads/profile");
+//
+//        request.getRequestDispatcher("/WEB-INF/ads/editads.jsp").forward(request, response);
+
+//        Long editAd = Long.parseLong(request.getParameter("editSingleAd"));
+//        Long editSingleAd = DaoFactory.getAdsDao().edit(editAd);
+        //editing ad right here
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        Long oneAd = Long.parseLong(request.getParameter("singleAd"));
+
+        Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
+
+        request.setAttribute("existingTitle", singleAd.getTitle());
+        request.setAttribute("existingDescription", singleAd.getDescription());
+        //grab ad by ID from MySqlAdsDao create a method
+
         System.out.println("singleAd.getTitle() = " + singleAd.getTitle());
         //ad that gets back set as an attribute do ${};
         request.setAttribute("singleAd", singleAd);
         request.setAttribute("existingTitle", singleAd.getTitle());
         request.setAttribute("existingDescription", singleAd.getDescription());
-        request.getRequestDispatcher("/WEB-INF/ads/editads.jsp").forward(request, response);
 
-    }
+        //======SETTING VALUES IN THE FORM=======//
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        if(request.getParameter("adToUpdate").equalsIgnoreCase("true")) {
+            DaoFactory.getAdsDao().edit(singleAd, title, description);
+            response.sendRedirect("/profile");
+        }
     }
 }
