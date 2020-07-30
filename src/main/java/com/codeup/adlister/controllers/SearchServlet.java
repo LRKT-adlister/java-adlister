dc209a9e1474c43d8386f3582e464dfdddb367df
Knoah Cotto <cotto.knoah@gmail.com>
2020-07-29 22:35:47 -0500

    search servlet

--- src/main/java/com/codeup/adlister/controllers/SearchServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchServlet.java
@@ -1,46 +1,30 @@
-//package com.codeup.adlister.controllers;
-//
-//import com.codeup.adlister.dao.DaoFactory;
-//import com.codeup.adlister.models.Ad;
-//import com.sun.tools.javac.util.List;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.sql.SQLException;
-//
-//
-//@WebServlet("/ad/search")
-//    public class SearchServlet extends HttpServlet {
-//
-//        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//            String searching = request.getParameter("search");
-//            List<Ad> ads = (List<Ad>) DaoFactory.getAdsDao().all();
-//            if (ads.equals(null)) //error if it can't be searched ?
-//
-////                get a list of all ads in database
-//
-//        try {
-//        List<Ad> adList = DaoFactory.getAdsDao().all(ads);
-//        request.setAttribute("ads", DaoFactory.getAdsDao().all(ads));
-//    } catch (SQLException se) {
-//        se.printStackTrace();
-//    }
-//        request.getSession().setAttribute("searched", "/ads");
-//        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
-//}
-//
-//        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//            request.getSession().setAttribute("searches","/index.jsp");
-//            request.getRequestDispatcher()
-//
-//
-//
-//    }
-//}
-//
-//
-//
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/ad/search")
+public class SearchServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String searchString = request.getParameter("search");
+        try {
+            List<Ad> adList = DaoFactory.getAdsDao().searchAds(searchString);
+            request.setAttribute("ads", DaoFactory.getAdsDao().searchAds(searchString));
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        request.getSession().setAttribute("marker", "/ads");
+        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+    }
+}
--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -1,113 +1,112 @@
-package com.codeup.adlister.dao;
-
-import com.codeup.adlister.models.Ad;
-import com.mysql.cj.jdbc.Driver;
-import models.Config;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class MySQLAdsDao implements Ads {
-    private Connection connection = null;
-
-    public MySQLAdsDao(Config config) {
-        try {
-            DriverManager.registerDriver(new Driver());
-            connection = DriverManager.getConnection(
-                config.getUrl(),
-                config.getUser(),
-                config.getPassword()
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException("Error connecting to the database!", e);
-        }
-    }
-
-    @Override
-    public Ad findUniqueAdId(Long ad){
-        String query = "SELECT * FROM ads WHERE id = ? LIMIT 1";
-        try{
-            PreparedStatement stmt = connection.prepareStatement(query);
-            stmt.setLong(1, ad);
-            ResultSet rs = stmt.executeQuery();
-            if (! rs.next()) {
-                return null;
-            }
-            return extractAd(rs);
-        } catch(SQLException e) {
-            throw new RuntimeException("Error finding Ad ID", e);
-        }
-    }
-
-    @Override
-    public List<Ad> all() {
-        PreparedStatement stmt = null;
-        try {
-            stmt = connection.prepareStatement("SELECT * FROM ads");
-            ResultSet rs = stmt.executeQuery();
-            return createAdsFromResults(rs);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error retrieving all ads.", e);
-        }
-    }
-
-    @Override
-    public Long insert(Ad ad) {
-        try {
-            String insertQuery = "INSERT INTO ads(user_id, title, description) VALUES (?, ?, ?)";
-            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, ad.getUserId());
-            stmt.setString(2, ad.getTitle());
-            stmt.setString(3, ad.getDescription());
-            stmt.executeUpdate();
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error creating a new ad.", e);
-        }
-    }
-    @Override
-    public void edit(Ad ad, String title, String description){
-        try{
-            String updateQuery = "UPDATE ads SET title = ?, description = ? WHERE id = ?";
-            PreparedStatement stmt = connection.prepareStatement(updateQuery, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1,title);
-            stmt.setString(2, description);
-            stmt.setLong(3, ad.getId());
-            stmt.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    @Override
-    public void delete(Ad ad, long id) {
-        try {
-            String deleteQuery = "DELETE FROM ads WHERE id = ?";
-            PreparedStatement stmt = connection.prepareStatement(deleteQuery, Statement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, id);
-            stmt.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    private Ad extractAd(ResultSet rs) throws SQLException {
-        return new Ad(
-            rs.getLong("id"),
-            rs.getLong("user_id"),
-            rs.getString("title"),
-            rs.getString("description")
-        );
-    }
-
-    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
-        List<Ad> ads = new ArrayList<>();
-        while (rs.next()) {
-            ads.add(extractAd(rs));
-        }
-        return ads;
-    }
-}
+//package com.codeup.adlister.dao;
+//
+//import com.codeup.adlister.models.Ad;
+//import com.mysql.cj.jdbc.Driver;
+//import models.Config;
+//import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public abstract class MySQLAdsDao implements Ads {
+//    private Connection connection = null;
+//
+//    public MySQLAdsDao(Config config) {
+//        try {
+//            DriverManager.registerDriver(new Driver());
+//            connection = DriverManager.getConnection(
+//                config.getUrl(),
+//                config.getUser(),
+//                config.getPassword()
+//            );
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error connecting to the database!", e);
+//        }
+//    }
+//
+//    @Override
+//    public Ad findUniqueAdId(Long ad){
+//        String query = "SELECT * FROM ads WHERE id = ? LIMIT 1";
+//        try{
+//            PreparedStatement stmt = connection.prepareStatement(query);
+//            stmt.setLong(1, ad);
+//            ResultSet rs = stmt.executeQuery();
+//            if (! rs.next()) {
+//                return null;
+//            }
+//            return extractAd(rs);
+//        } catch(SQLException e) {
+//            throw new RuntimeException("Error finding Ad ID", e);
+//        }
+//    }
+//
+//    @Override
+//    public List<Ad> all() {
+//        PreparedStatement stmt = null;
+//        try {
+//            stmt = connection.prepareStatement("SELECT * FROM ads");
+//            ResultSet rs = stmt.executeQuery();
+//            return createAdsFromResults(rs);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error retrieving all ads.", e);
+//        }
+//    }
+//
+//    @Override
+//    public Long insert(Ad ad) {
+//        try {
+//            String insertQuery = "INSERT INTO ads(user_id, title, description) VALUES (?, ?, ?)";
+//            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+//            stmt.setLong(1, ad.getUserId());
+//            stmt.setString(2, ad.getTitle());
+//            stmt.setString(3, ad.getDescription());
+//            stmt.executeUpdate();
+//            ResultSet rs = stmt.getGeneratedKeys();
+//            rs.next();
+//            return rs.getLong(1);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error creating a new ad.", e);
+//        }
+//    }
+//    @Override
+//    public void edit(Ad ad, String title, String description){
+//        try{
+//            String updateQuery = "UPDATE ads SET title = ?, description = ? WHERE id = ?";
+//            PreparedStatement stmt = connection.prepareStatement(updateQuery, Statement.RETURN_GENERATED_KEYS);
+//            stmt.setString(1,title);
+//            stmt.setString(2, description);
+//            stmt.setLong(3, ad.getId());
+//            stmt.executeUpdate();
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void delete(Ad ad, long id) {
+//        try {
+//            String deleteQuery = "DELETE FROM ads WHERE id = ?";
+//            PreparedStatement stmt = connection.prepareStatement(deleteQuery, Statement.RETURN_GENERATED_KEYS);
+//            stmt.setLong(1, id);
+//            stmt.executeUpdate();
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//    }
+//
+//    private Ad extractAd(ResultSet rs) throws SQLException {
+//        return new Ad(
+//            rs.getLong("id"),
+//            rs.getLong("user_id"),
+//            rs.getString("title"),
+//            rs.getString("description")
+//        );
+//    }
+//
+//    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+//        List<Ad> ads = new ArrayList<>();
+//        while (rs.next()) {
+//            ads.add(extractAd(rs));
+//        }
+//        return ads;
+//    }
+//}
