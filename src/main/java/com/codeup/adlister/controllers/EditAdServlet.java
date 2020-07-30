2ce23bbe81ea213edb304a9022f9d8d1d62d8786
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-07-30 08:03:17 -0500

    cleaning up editAdServlet and other classes

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -16,7 +16,6 @@ import static java.lang.Long.parseLong;
 public class EditAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//        if(request.getSession().setAttribute("Ad", ))
         String adId = request.getPathInfo().substring(1);
 
         Ad grabId = DaoFactory.getAdsDao().findUniqueAdId(parseLong(adId));
@@ -25,55 +24,13 @@ public class EditAdServlet extends HttpServlet {
 
         System.out.println("grabId.getDescription() = " + grabId.getDescription());
         System.out.println("grabId.getDescription() = " + grabId.getTitle());
-//
-//        Long oneAd = parseLong(request.getParameter("singleAd"));
-//        Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
+
 
         request.setAttribute("existingTitle", grabId.getTitle());
         request.setAttribute("existingDescription", grabId.getDescription());
 
-
-        //grab ad by ID from MySqlAdsDao create a method
-//        Long oneAd = Long.parseLong(request.getParameter("singleAd"));
-//
-//            Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
-//            System.out.println("singleAd.getTitle() = " + singleAd.getTitle());
-//            //ad that gets back set as an attribute do ${};
-//            request.setAttribute("singleAd", singleAd);
-//            request.setAttribute("existingTitle", singleAd.getTitle());
-//            request.setAttribute("existingDescription", singleAd.getDescription());
-//
-//            //======SETTING VALUES IN THE FORM=======//
-//            String title = request.getParameter("title");
-//            String description = request.getParameter("description");
-//
-//
-//            DaoFactory.getAdsDao().edit(singleAd, title, description);
-//            response.sendRedirect("/ads/profile");
-//
             request.getRequestDispatcher("/WEB-INF/ads/editads.jsp").forward(request, response);
 
-
-//        Ad singleAd = DaoFactory.getAdsDao().findUniqueAdId(oneAd);
-//        System.out.println("singleAd.getTitle() = " + singleAd.getTitle());
-//        //ad that gets back set as an attribute do ${};
-//        request.setAttribute("singleAd", singleAd);
-//        request.setAttribute("existingTitle", singleAd.getTitle());
-//        request.setAttribute("existingDescription", singleAd.getDescription());
-//
-//        //======SETTING VALUES IN THE FORM=======//
-//        String title = request.getParameter("title");
-//        String description = request.getParameter("description");
-//
-//
-//        DaoFactory.getAdsDao().edit(singleAd, title, description);
-//        response.sendRedirect("/ads/profile");
-//
-//        request.getRequestDispatcher("/WEB-INF/ads/editads.jsp").forward(request, response);
-
-//        Long editAd = Long.parseLong(request.getParameter("editSingleAd"));
-//        Long editSingleAd = DaoFactory.getAdsDao().edit(editAd);
-        //editing ad right here
 
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -81,21 +38,16 @@ public class EditAdServlet extends HttpServlet {
 
         System.out.println("HELLLO HELP MEEEEEEE PLEASE IM DYINGGGGGGGG");
 
-//        Long oneAd = parseLong(request.getParameter("ad"));
 
 
         Ad adObject = (Ad) request.getSession().getAttribute("ad");
-//                DaoFactory.getAdsDao().findUniqueAdId(oneAd);
 
         System.out.println("singleAd = " + adObject);
-//        System.out.println("oneAd = " + oneAd);
 
         request.setAttribute("existingTitle", adObject.getTitle());
         request.setAttribute("existingDescription", adObject.getDescription());
-        //grab ad by ID from MySqlAdsDao create a method
 
         System.out.println("adObject.getTitle() = " + adObject.getTitle());
-        //ad that gets back set as an attribute do ${};
         request.setAttribute("adObject", adObject);
         request.setAttribute("existingTitle", adObject.getTitle());
         request.setAttribute("existingDescription", adObject.getDescription());
